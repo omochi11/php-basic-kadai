7f1fdd8c61e76bcfbf0c19f156d3e50eb0b7d4a5
omochi11 <152568172+omochi11@users.noreply.github.com>
2024-01-05 16:21:59 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <?PHP
+    // 連想配列に値を入力する
+    $Array = ['name' => 'onion', 'price' => 200,' weight' => 160,];
+    // 連想配列の値を出力する
+    print_r($Array);
+    ?>
+</body>
+
+</html>
